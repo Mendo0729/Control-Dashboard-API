7a3354447750fcd1a6a7fb0589ef1604dbe66121
Abdiel Mendoza <abdielmemdoza2901@gmail.com>
2026-07-29 14:29:21 -0500

    feat(config): initialize global configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import { validateEnvironment } from './config/env.validation';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      expandVariables: true,
+      load: [appConfig, databaseConfig],
+      validate: validateEnvironment,
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
