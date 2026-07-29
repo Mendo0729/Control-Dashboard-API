06f8151aa6d88c929ab88edb51a3d80d340b9419
Abdiel Mendoza <abdielmemdoza2901@gmail.com>
2026-07-29 16:02:52 -0500

    feat(projects): add create project DTO

---
+++ src/projects/dto/create-project.dto.ts
@@ -0,0 +1,62 @@
+import {
+  IsBoolean,
+  IsIn,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import {
+  PROJECT_STATUSES,
+  ProjectStatus,
+} from '../project-status.constants';
+
+export class CreateProjectDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(150)
+  name!: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(160)
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message:
+      'slug solo puede contener letras minúsculas, números y guiones simples.',
+  })
+  slug?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(5000)
+  description?: string;
+
+  @IsOptional()
+  @IsIn(PROJECT_STATUSES)
+  status?: ProjectStatus;
+
+  @IsOptional()
+  @IsBoolean()
+  visibilityPublic?: boolean;
+
+  @IsOptional()
+  @IsUrl({ require_protocol: true })
+  @MaxLength(2048)
+  productionUrl?: string;
+
+  @IsOptional()
+  @IsUrl({ require_protocol: true })
+  @MaxLength(2048)
+  stagingUrl?: string;
+
+  @IsOptional()
+  @IsUrl({ require_protocol: true })
+  @MaxLength(2048)
+  logoUrl?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  featured?: boolean;
+}
