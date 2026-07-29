755b732319bc43c7eecbc2bef07395ecf08d51f4
Abdiel Mendoza <abdielmemdoza2901@gmail.com>
2026-07-29 16:04:20 -0500

    feat(projects): add projects module

---
+++ src/projects/projects.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { DatabaseModule } from '../database/database.module';
+import { ProjectsController } from './projects.controller';
+import { ProjectsService } from './projects.service';
+
+@Module({
+  imports: [AuthModule, DatabaseModule],
+  controllers: [ProjectsController],
+  providers: [ProjectsService],
+  exports: [ProjectsService],
+})
+export class ProjectsModule {}
